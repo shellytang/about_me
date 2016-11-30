091d5a272d7ceb49803eb96a0dce092463c888fe
Shelly Tang <shellytang@gmail.com>
2016-11-29 17:42:11 -0800

    added console log to print user answers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var response1 = prompt('Does Shelly have any cats?').toLowerCase();
+console.log('Does Shelly have any cats? The user answered: ' + response1);
 
 if (response1 === 'yes' || response1 === 'y') {
   alert('Yes, Shelly does have two cats!');
@@ -10,7 +11,9 @@ if (response1 === 'yes' || response1 === 'y') {
   alert('Try to answer with a yes/y or no/n next time.');
 }
 
+
 var response2 = prompt('Does Shelly have any dogs?').toLowerCase();
+console.log('Does Shelly have any dogs? The user answered: ' + response2);
 
 if (response2 === 'yes' || response2 === 'y') {
   alert('Sorry, you\'re wrong, Shelly does not have any dogs.')
@@ -20,7 +23,9 @@ if (response2 === 'yes' || response2 === 'y') {
   alert('Try to answer with a yes/y or no/n next time.');
 }
 
+
 var response3 = prompt('Does Shelly have any siblings?').toLowerCase();
+console.log('Does Shelly have any siblings? The user answered: ' + response3);
 
 if (response3 === 'yes' || response3 === 'y') {
   alert('Yes, Shelly has a sister.');
@@ -30,7 +35,9 @@ if (response3 === 'yes' || response3 === 'y') {
   alert('Try to answer with a yes/y or no/n next time.');
 }
 
+
 var response4 = prompt('Has Shelly been to Iceland?').toLowerCase();
+console.log('Has Shelly been to Iceland? The user answered: ' + response4);
 
 if (response4 === 'yes' || response4 === 'y') {
   alert('Sorry, you\'re wrong, she has never visited Iceland.');
@@ -40,7 +47,9 @@ if (response4 === 'yes' || response4 === 'y') {
   alert('Try to answer with a yes/y or no/n next time.');
 }
 
+
 var response5 = prompt('Has Shelly been to Asia?').toLowerCase();
+console.log('Has Shelly been to Asia? The user answered: ' + response5);
 
 if (response5 === 'yes' || response5 === 'y') {
   alert('You\'re right, she has visited Japan and China.');
