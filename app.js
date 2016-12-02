75dffc3f02e8e3a615b9f1991ce7561bf6fbf03d
Shelly Tang <shellytang@gmail.com>
2016-12-01 16:11:33 -0800

    renamed variables so they were more descriptive and added intro alert

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 'use strict';
-function question1(){
-  var response1 = prompt('Does Shelly have any cats?').toLowerCase();
-  console.log('Does Shelly have any cats? The user answered: ' + response1);
 
-  if (response1 === 'yes' || response1 === 'y') {
+alert("Welcome! We are going to play a guessing game, where you will learn more about me. Please provide your responses with yes/no or y/n.");
+
+function question1(){
+  var cats = prompt('Does Shelly have any cats?').toLowerCase();
+  console.log('Does Shelly have any cats? The user answered: ' + cats);
+
+  if (cats === 'yes' || cats === 'y') {
     alert('Yes, Shelly does have two cats!');
-  } else if (response1 === 'no' || response1 === 'n') {
+  } else if (cats === 'no' || cats === 'n') {
     alert('Sorry, you\'re wrong, Shelly has two cats.');
   } else {
     alert('Try to answer with a yes/y or no/n next time.');
@@ -14,12 +17,12 @@ function question1(){
 question1();
 
 function question2(){
-  var response2 = prompt('Does Shelly have any dogs?').toLowerCase();
-  console.log('Does Shelly have any dogs? The user answered: ' + response2);
+  var dogs = prompt('Does Shelly have any dogs?').toLowerCase();
+  console.log('Does Shelly have any dogs? The user answered: ' + dogs);
 
-  if (response2 === 'yes' || response2 === 'y') {
+  if (dogs === 'yes' || dogs === 'y') {
     alert('Sorry, you\'re wrong, Shelly does not have any dogs.')
-  } else if (response2 === 'no' || response2 === 'n') {
+  } else if (dogs === 'no' || dogs === 'n') {
     alert('Correct, she does not have any dogs, but she wants one!');
   } else {
     alert('Try to answer with a yes/y or no/n next time.');
@@ -28,12 +31,12 @@ function question2(){
 question2();
 
 function question3(){
-  var response3 = prompt('Does Shelly have any siblings?').toLowerCase();
-  console.log('Does Shelly have any siblings? The user answered: ' + response3);
+  var siblings = prompt('Does Shelly have any siblings?').toLowerCase();
+  console.log('Does Shelly have any siblings? The user answered: ' + siblings);
 
-  if (response3 === 'yes' || response3 === 'y') {
+  if (siblings === 'yes' || siblings === 'y') {
     alert('Yes, Shelly has a sister.');
-  } else if (response3 === 'no' || response3 === 'n') {
+  } else if (siblings === 'no' || siblings === 'n') {
     alert('Sorry, you\'re wrong, she has a sister.');
   } else {
     alert('Try to answer with a yes/y or no/n next time.');
@@ -42,12 +45,12 @@ function question3(){
 question3();
 
 function question4(){
-  var response4 = prompt('Has Shelly been to Iceland?').toLowerCase();
-  console.log('Has Shelly been to Iceland? The user answered: ' + response4);
+  var iceland = prompt('Has Shelly been to Iceland?').toLowerCase();
+  console.log('Has Shelly been to Iceland? The user answered: ' + iceland);
 
-  if (response4 === 'yes' || response4 === 'y') {
+  if (iceland === 'yes' || iceland === 'y') {
     alert('Sorry, you\'re wrong, she has never visited Iceland.');
-  } else if (response4 === 'no' || response4 === 'n') {
+  } else if (iceland === 'no' || iceland === 'n') {
     alert('Correct, she has never been, but she wants to go one day!');
   } else {
     alert('Try to answer with a yes/y or no/n next time.');
@@ -56,12 +59,12 @@ function question4(){
 question4();
 
 function question5(){
-  var response5 = prompt('Has Shelly been to Asia?').toLowerCase();
-  console.log('Has Shelly been to Asia? The user answered: ' + response5);
+  var asia = prompt('Has Shelly been to Asia?').toLowerCase();
+  console.log('Has Shelly been to Asia? The user answered: ' + asia);
 
-  if (response5 === 'yes' || response5 === 'y') {
+  if (asia === 'yes' || asia === 'y') {
     alert('You\'re right, she has visited Japan and China.');
-  } else if (response5 === 'no' || response5 === 'n') {
+  } else if (asia === 'no' || asia === 'n') {
     alert('Sorry, you are wrong, she has visited Japan and China.');
   }
 }
@@ -71,12 +74,12 @@ function question6(){
   var myBusNumber = 8;
 
   for (var i = 0; i < 4; i++) {
-    var response6 = parseInt(prompt('What bus did Shelly take to class today?'));
-    if (response6 > myBusNumber) {
+    var bus = parseInt(prompt('What bus did Shelly take to class today?'));
+    if (bus > myBusNumber) {
       alert('You guessed too high!');
-    } else if (response6 < myBusNumber) {
+    } else if (bus < myBusNumber) {
       alert('You guessed too low!');
-    } else if (isNaN(response6)) {
+    } else if (isNaN(bus)) {
       alert('That is not a number');
     } else {
       alert('Good job!');
@@ -92,8 +95,8 @@ function question7(){
 
   for (var j = 0; j < thanksgivingDinner.length; j++) {
     while (guesses < 6) {
-      var response7 = prompt('What did I eat for Thanksgiving?');
-      if (response7 === thanksgivingDinner[j]) {
+      var thanksgivingQ = prompt('What did I eat for Thanksgiving?');
+      if (thanksgivingQ === thanksgivingDinner[j]) {
         alert('You are right!');
         guesses = 6;
       } else {
